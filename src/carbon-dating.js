6126e4dea156cf29da20d29a0bf661c03214d922
Nonsmokers <Vasyuxnevichs@mail.ru>
2020-03-22 22:55:31 +0300

    Feat: add solution carbon

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,14 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  let number = parseFloat(sampleActivity);
+
+  if (typeof sampleActivity !== 'string') {
+    return false;
+    } else if (number < MODERN_ACTIVITY && number > 0) {
+    return Math.ceil(Math.log(MODERN_ACTIVITY / number) * HALF_LIFE_PERIOD / 0.693);
+    } else {
+    return false;
+  }
 };
